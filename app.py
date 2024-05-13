d7b4a34cdbbcb225e7f1c91eb6f24074bb0d1261
Shieliang <shieliang22@gmail.com>
2024-05-13 12:21:04 +0800

    Data Encrypt and Decrypt

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import *
+from cryptography.fernet import Fernet
 import csv
+
+key = Fernet.generate_key()
+with open('key.key', 'wb') as key_file:
+    key_file.write(key)
+
+def is_valid_fernet_token(token, cipher_suite):
+    try:
+        cipher_suite.decrypt(token.encode())
+        return True
+    except:
+        return False
 
 app = Flask(__name__)
 app.secret_key = 'abcdefgabcdabcdabcd'
+
 
 @app.route('/')
 def index():
@@ -15,6 +28,24 @@ def submit_feedback():
     email = request.form['email']
     comments = request.form['comments']
     rating = request.form['rating']
+
+    with open('key.key', 'rb') as key_file:
+        key = key_file.read()
+
+    f = Fernet(key)
+
+    name = f.encrypt(name.encode('utf-8')).decode()
+    gender = f.encrypt(gender.encode('utf-8')).decode()
+    email = f.encrypt(email.encode('utf-8')).decode()
+    comments = f.encrypt(comments.encode('utf-8')).decode()
+    rating = f.encrypt(rating.encode('utf-8')).decode()
+
+    print(name)
+    print(f.decrypt(name).decode('utf-8'))
+    print(f.decrypt(gender).decode('utf-8'))
+    print(f.decrypt(email).decode('utf-8'))
+    print(f.decrypt(comments).decode('utf-8'))
+    print(f.decrypt(rating).decode('utf-8'))
 
     with open('feedback.csv', 'a', newline='') as csvfile:
         fieldnames = ['Name', 'Gender', 'Email', 'Comments', 'Rating']
@@ -40,6 +71,11 @@ def admin_feedback():
     if not session.get('admin_logged_in'):
         return redirect(url_for('admin_login'))
 
+    # Load the key
+    with open('key.key', 'rb') as key_file:
+        key = key_file.read()
+
+    f = Fernet(key)
     # Read feedback data from CSV file
     feedback_data = []
     with open('feedback.csv', 'r') as csvfile:
@@ -54,6 +90,12 @@ def admin_feedback():
                     'Comments': row[3],   # Fourth column
                     'Rating': row[4]      # Fifth column
                 }
+                for key in mapped_row:
+                    try:
+                        mapped_row[key] = f.decrypt(mapped_row[key].encode()).decode('utf-8')
+                    except:
+                        mapped_row[key] = 'Invalid token'
+
                 feedback_data.append(mapped_row)
 
     print(feedback_data)  # Debugging output
